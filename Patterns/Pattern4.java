40138f751f715df85d6bdd4f64f9f267c02338c8
nitish-devop <devnitish97@gmail.com>
2023-03-21 10:15:10 -0700

    Basic pattern added

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+/* 
+Input Format: N = 6
+Result:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+ */
+public class Pattern4 {
+    public static void main(String[] args) {
+        int row = 4;
+        int col = 4;
+
+        for(int i=1;i<=row;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
